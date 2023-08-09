2ce6e52317265fb4291a9d9c3d250d208339d0bd
Clearsu <clearwater1219@gmail.com>
2023-08-09 18:49:54 +0900

    lecture 27 done

---
+++ React/27_Control.js
@@ -0,0 +1,13 @@
+// const password = prompt('Your password');
+
+// if (password == 1234) {
+//     console.log("Correct!");
+// } else {
+//     console.log("Fail!");
+// }
+
+const hobbies = ["Sport", "Game", "Music"];
+
+for (const hobby of hobbies) {
+    console.log(hobby);
+}
