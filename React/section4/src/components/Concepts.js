4f58dde6ea57ca0264c4f25307f0aef24428cbca
Clearsu <clearwater1219@gmail.com>
2023-08-11 21:26:44 +0900

    module04 done

---
+++ React/section4/src/components/Concepts.js
@@ -0,0 +1,25 @@
+import Concept from "./Concept";
+
+import "../index.css";
+
+export default function Concepts(props) {
+  return (
+    <ul id="concepts">
+      <Concept
+        image={props.concepts[0].image}
+        title={props.concepts[0].title}
+        description={props.concepts[0].description}
+      />
+      <Concept
+        image={props.concepts[1].image}
+        title={props.concepts[1].title}
+        description={props.concepts[1].description}
+      />
+      <Concept
+        image={props.concepts[2].image}
+        title={props.concepts[2].title}
+        description={props.concepts[2].description}
+      />
+    </ul>
+  );
+}
